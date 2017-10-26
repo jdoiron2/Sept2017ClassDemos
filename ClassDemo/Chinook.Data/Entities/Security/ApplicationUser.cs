1c867ccc9bb35368484e5b83d7f90a38704e17ac
jdoiron2 <31717383+jdoiron2@users.noreply.github.com>
2017-10-25 20:50:58 -0600

    In Class Changes

--- ClassDemo/Chinook.Data/Entities/Security/ApplicationUser.cs
+++ ClassDemo/Chinook.Data/Entities/Security/ApplicationUser.cs
@@ -11,5 +11,6 @@ namespace Chinook.Data.Entities.Security
     {
         public int? EmployeeId { get; set; }
         public int? CustomerID { get; set; }
+        public object EmployeeID { get; set; }
     }
 }
