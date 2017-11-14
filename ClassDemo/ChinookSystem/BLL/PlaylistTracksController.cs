01afec72cdb5781f06cf540f03d61459dc86351d
jdoiron2 <31717383+jdoiron2@users.noreply.github.com>
2017-11-13 20:49:54 -0700

    In class changes
    Adding an item to the playlist

--- ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
+++ ClassDemo/ChinookSystem/BLL/PlaylistTracksController.cs
@@ -21,19 +21,101 @@ namespace ChinookSystem.BLL
         {
             using (var context = new ChinookContext())
             {
-               
-                //code to go here
 
-                return null;
+                //what would happen if there is no match for the
+                // incoming parameter values
+                //we need to ensure that the results have a valid value
+                //this value will be the result of a query either a null (not found)
+                // of an IEnumerable<T> collection
+                //to achieve a valid value encapsulate the query in a 
+                //  .FirstOFDefault()
+                var results = (from x in context.Playlists
+                              where x.UserName.Equals(username)
+                              && x.Name.Equals(playlistname)
+                              select x).FirstOrDefault();
+
+               //test if you should return a null as your collection
+               //   or find the tracks for the given PlaylistId in the results
+                if (results == null)
+                {
+                    return null;
+                }
+                else
+                {
+                //now get the tracks
+                var theTracks = from x in context.PlaylistTracks
+                                where x.Playlist.Equals(results.PlaylistId)
+                                orderby x.TrackNumber
+                                select new UserPlaylistTrack
+                                {
+                                    TrackID = x.TrackId,
+                                    TrackNumber = x.TrackNumber,
+                                    TrackName = x.Track.Name,
+                                    Milliseconds = x.Track.Milliseconds,
+                                    UnitPrice = x.Track.UnitPrice
+                                };
+                
+                return theTracks.ToList();
+                }
             }
         }//eom
-        public void Add_TrackToPLaylist(string playlistname, string username, int trackid)
+        public List<UserPlaylistTrack> Add_TrackToPLaylist(string playlistname, string username, int trackid)
         {
             using (var context = new ChinookContext())
             {
                 //code to go here
-                
-             
+                //Part One:
+                //query to get the Playlist id
+                var exists = (from x in context.Playlists
+                               where x.UserName.Equals(username)
+                               && x.Name.Equals(playlistname)
+                               select x).FirstOrDefault();
+                //intialize the tracknumber
+                int tracknumber = 0;
+                //I will need to create an instance of PlayListTrack
+                PlaylistTrack newTrack = null;
+
+                //determine if a playlist "parent" instance needs to be created
+                if (exists == null)
+                {
+                    //this is a new PlayList
+                    //create an instance of playlist to add to PlayList table
+                    exists = new Playlist();
+                    exists.Name = playlistname;
+                    exists.UserName = username;
+                    exists = context.Playlists.Add(exists);
+                    //at this time there is NO physical PKey
+                    //the psuedo PKey is handled by the HashSet
+                    tracknumber = 1;
+                }
+                else
+                {
+                    //playlist exists
+                    //I need to generate the next TrackNumber
+                    tracknumber = exists.PlaylistTracks.Count() + 1;
+
+                    //validation: om our example a track can only exist once in a particular playlist
+                    newTrack = exists.PlaylistTracks.SingleOrDefault(x => x.TrackId == trackid);
+                    if (newTrack != null)
+                    {
+                        throw new Exception("Playlist already has requested track");
+                    }
+                }
+
+                //Part Two: Add the PlayListTrack instance
+                //use naviagtion to .Add the new track to PlaylistTrack
+                newTrack = new PlaylistTrack();
+                newTrack.TrackId = trackid;
+                newTrack.TrackNumber = tracknumber;
+
+                //NOTE: the pkey for the PlaylistId may not yet exist
+                //  USING navigation one can let HashSet handle the PlaylistId
+                // pkey value
+                exists.PlaylistTracks.Add(newTrack);
+
+                //physically add all data to the database
+                context.SaveChanges();
+                return List_TracksForPlaylist(playlistname, username);
             }
         }//eom
         public void MoveTrack(string username, string playlistname, int trackid, int tracknumber, string direction)
--- ClassDemo/Website/SamplePages/ManagePlaylist.aspx.cs
+++ ClassDemo/Website/SamplePages/ManagePlaylist.aspx.cs
@@ -111,6 +111,33 @@ public partial class SamplePages_ManagePlaylist : System.Web.UI.Page
         ListViewCommandEventArgs e)
     {
         //code to go here
+        if (String.IsNullOrEmpty(PlaylistName.Text))
+        {
+            MessageUserControl.ShowInfo("Warning", "PlayList Name is Required.");
+        }
+        else
+        {
+            string username = User.Identity.Name;
+
+            //where does trackid come from
+            // ListViewCommandEventArgs e contains the parameter values for this event;
+            // CommandArgument
+            //CommandArgument is an object
+            int trackid = int.Parse(e.CommandArgument.ToString());
+
+            //send you collection of parameter values to the BLL for processing
+            MessageUserControl.TryRun(() =>
+                {
+                    PlaylistTracksController sysmgr = new PlaylistTracksController();
+                    List<UserPlaylistTrack> refreshResults = sysmgr.Add_TrackToPLaylist(PlaylistName.Text, username, trackid);
+                    PlayList.DataSource = refreshResults;
+                    PlayList.DataBind();
+            },"Success","Your track has been added to your Playlist");
+
+
+
+        }
+
     }
 
     protected void MoveUp_Click(object sender, EventArgs e)
